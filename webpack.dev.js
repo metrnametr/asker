01772cbf4d3fa7e231b314b381fd671b16159353
metrnametr <patronius1515@gmail.com>
2020-04-15 18:25:12 +0300

    otimize img

--- webpack.dev.js
+++ webpack.dev.js
@@ -1,5 +1,6 @@
 const path = require('path');
 const HTMLWebpackPlugin = require('html-webpack-plugin');
+const ImageminPlugin = require('imagemin-webpack-plugin').default;
 
 module.exports = {
     entry: './src/index.js',
@@ -39,6 +40,11 @@ module.exports = {
     plugins: [
         new HTMLWebpackPlugin({
             template: './src/index.html'
+        }),
+        new ImageminPlugin({
+            pngquant: {
+              quality: '95-100'
+            }
         })
     ]
 }
