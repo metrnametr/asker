7062c80a904fd8426ad2792a05582b27de6205d2
metrnametr <patronius1515@gmail.com>
2020-04-15 17:34:06 +0300

    add responsive

--- src/index.js
+++ src/index.js
@@ -6,6 +6,18 @@ const collapsedItems = document.querySelectorAll('.collapse');
 
 
 collapsedItems.forEach(it => it.addEventListener('click', function(e) {
-    e.preventDefault();
-    this.classList.toggle('active');
+    const { dataset } = e.target
+    if (dataset.action === "toggle") {
+        e.preventDefault();
+        this.classList.toggle('active');
+    }
 }))
+
+
+const buttonToggleMenu = document.querySelector('.toggle-menu');
+
+
+buttonToggleMenu.addEventListener('click', () => {
+    const menu = document.querySelector('.nav-container');
+    menu.classList.toggle('active');
+})
